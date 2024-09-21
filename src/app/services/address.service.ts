aeca8f89312f626fff686818a1d11b39958a7da9
Ghostman312 <57714309+Ghostman312@users.noreply.github.com>
2024-09-21 13:43:00 +0200

    Adding constants and beginning the implementation of needed functions into address.service.ts

--- src/app/services/address.service.ts
+++ src/app/services/address.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { BASE_URL, ADDRESSES_ENDPOINT } from './constants';
+import {throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getById(id: number) {
+    return this.http.get(`${BASE_URL}${ADDRESSES_ENDPOINT}/${id}`).pipe(
+      catchError(error => {
+        console.error('Error occurred:', error);
+        return throwError(() => new Error(error));
+      })
+    );
+  }
 }
---
+++ src/app/services/constants.ts
@@ -0,0 +1,17 @@
+import secrets from '../../secrets.json'
+
+export const BASE_URL: string = `http://${secrets.ipAddress}:8080/api/v1`;
+export const USERS_ENDPOINT = '/users';
+export const PUBLICATION_ENDPOINT: string = '/publications';
+export const ADDRESSES_ENDPOINT: string = '/addresses';
+export const COMMENTS_ENDPOINT: string = '/comments';
+export const MESSAGES_ENDPOINT: string = '/messages';
+export const PICTURES_ENDPOINT: string = '/pictures';
+export const PLANT_CONDITIONS_ENDPOINT: string = '/plantsConditions';
+export const PLANTS_ENDPOINT: string = '/plants';
+export const REPORTS_ENDPOINT: string = '/reports';
+export const USER_TYPES_ENDPOINT: string = '/user_types';
+export const REGISTER_ENDPOINT: string = '/auth/register';
+export const AUTHENTICATE_ENDPOINT: string = '/auth/authenticate';
+export const REFRESH_ENDPOINT: string = '/auth/refresh-token';
+
